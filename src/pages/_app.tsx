1c8854b92ced98473d8b05d0a3e244e7392dafc3
k-logic24 <k.log.ic563@gmail.com>
2022-08-07 21:20:23 +0900

    ディレクトリ整理 & Mantine UI導入

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+
+import '@/styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
+}
+
+export default MyApp
