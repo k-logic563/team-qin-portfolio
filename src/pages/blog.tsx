fc1a925ce1446531d4d819b00caf546127403d51
k-logic24 <k.log.ic563@gmail.com>
2022-08-23 22:20:31 +0900

    getserversideへ

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,4 +1,4 @@
-import { NextPage, InferGetStaticPropsType } from 'next'
+import { NextPage, InferGetServerSidePropsType } from 'next'
 
 import { Blog } from '@/features/Blog'
 import { AppHead } from '@/components/Element/Head'
@@ -6,16 +6,15 @@ import { AppHead } from '@/components/Element/Head'
 import { client } from '@/lib/axios'
 import { BlogContent } from '@/types'
 
-export type BlogProps = InferGetStaticPropsType<typeof getStaticProps>
+export type BlogProps = InferGetServerSidePropsType<typeof getServerSideProps>
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const { data } = await client.get<BlogContent>('blogs')
 
   return {
     props: {
       response: data,
     },
-    revalidate: 10,
   }
 }
 
