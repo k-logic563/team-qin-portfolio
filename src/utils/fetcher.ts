1c0160318ff7914617ccb774ea49f547373cc5ba
k-logic24 <k.log.ic563@gmail.com>
2022-09-11 16:39:30 +0900

    baseurlは不要

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,7 +1,3 @@
 import axios from 'axios'
 
-const http = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-})
-
-export const fetcher = (url: string) => http.get(url).then((res) => res.data)
+export const fetcher = (url: string) => axios.get(url).then((res) => res.data)
