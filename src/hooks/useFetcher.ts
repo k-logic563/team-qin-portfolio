0d08aa2eda5f78c7cd0a52b270a41e4ff54d3818
k-logic24 <k.log.ic563@gmail.com>
2022-09-15 07:42:50 +0900

    baseurl設定

--- src/hooks/useFetcher.ts
+++ src/hooks/useFetcher.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import { fetcher } from '@/utils/fetcher'
 
 export const useFetcher = <T>(slug: string) => {
-  const { data, error } = useSWR<T>(`/api/${slug}`, fetcher, {
+  const { data, error } = useSWR<T>(`${process.env.NEXT_PUBLIC_BASE_URL}/api/${slug}`, fetcher, {
     suspense: true,
   })
 
