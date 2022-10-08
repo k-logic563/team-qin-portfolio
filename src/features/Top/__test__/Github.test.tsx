5713aecf9baee59b09a4589ce1c01d00a92b69ed
k-logic24 <k.log.ic563@gmail.com>
2022-10-08 23:57:56 +0900

    テストリファクタリング

--- src/features/Top/__test__/Github.test.tsx
+++ src/features/Top/__test__/Github.test.tsx
@@ -1,80 +1,35 @@
-import { Suspense } from 'react'
 import { render } from '@testing-library/react'
 import { SWRResponse, SWRConfig, Middleware } from 'swr'
 
 import { Github } from '@/features/Top/Github'
-import { ErrorWrapper } from '@/components/Element/Error/ErrorWrapper'
 
 import { formatGithubLanguages } from '@/utils/format'
 import { props, mockData } from './mocks/github'
 
-describe('GitHubデータ取得、表示', () => {
-  it('正常にデータが反映される', () => {
-    const middleware: Middleware = () => {
-      return (): SWRResponse<any, any> => {
-        return {
-          data: mockData,
-          error: undefined,
-          mutate: (_) => Promise.resolve(),
-          isValidating: false,
-        }
+test('GitHubデータ取得、表示', () => {
+  const middleware: Middleware = () => {
+    return (): SWRResponse<any, any> => {
+      return {
+        data: mockData,
+        error: undefined,
+        mutate: (_) => Promise.resolve(),
+        isValidating: false,
       }
     }
+  }
 
-    const { getByText } = render(
-      <ErrorWrapper message="failed to fetch">
-        <Suspense fallback={<p>loading</p>}>
-          <SWRConfig value={{ use: [middleware] }}>
-            <Github />
-          </SWRConfig>
-        </Suspense>
-      </ErrorWrapper>
-    )
+  const { getByText } = render(
+    <SWRConfig value={{ use: [middleware] }}>
+      <Github />
+    </SWRConfig>
+  )
 
-    const language = formatGithubLanguages(mockData)[0][0]
+  const language = formatGithubLanguages(mockData)[0][0]
 
-    expect(getByText(props.name)).toBeInTheDocument()
-    expect(getByText(props.description)).toBeInTheDocument()
-    expect(getByText(props.forkCount)).toBeInTheDocument()
-    expect(getByText(props.stargazerCount)).toBeInTheDocument()
-    expect(getByText(props.languageName)).toBeInTheDocument()
-    expect(getByText(`${language.value}%`)).toBeInTheDocument()
-  })
-
-  it('エラー表示', () => {
-    const middleware: Middleware = () => {
-      return (): SWRResponse<any, any> => {
-        return {
-          data: undefined,
-          error: Error(),
-          mutate: (_) => Promise.resolve(),
-          isValidating: false,
-        }
-      }
-    }
-
-    const { getByText } = render(
-      <ErrorWrapper message="failed to fetch">
-        <Suspense fallback={<p>loading</p>}>
-          <SWRConfig value={{ use: [middleware] }}>
-            <Github />
-          </SWRConfig>
-        </Suspense>
-      </ErrorWrapper>
-    )
-
-    expect(getByText('failed to fetch')).toBeInTheDocument()
-  })
-
-  it('データ取得待機中', () => {
-    const { getByText } = render(
-      <ErrorWrapper message="failed to fetch">
-        <Suspense fallback={<p>loading</p>}>
-          <Github />
-        </Suspense>
-      </ErrorWrapper>
-    )
-
-    expect(getByText('loading')).toBeInTheDocument()
-  })
+  expect(getByText(props.name)).toBeInTheDocument()
+  expect(getByText(props.description)).toBeInTheDocument()
+  expect(getByText(props.forkCount)).toBeInTheDocument()
+  expect(getByText(props.stargazerCount)).toBeInTheDocument()
+  expect(getByText(props.languageName)).toBeInTheDocument()
+  expect(getByText(`${language.value}%`)).toBeInTheDocument()
 })
